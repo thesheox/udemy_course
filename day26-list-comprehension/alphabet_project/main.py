5f530719334b76b8b9c5550527ce8f4d5528b99d
thesheox <thesheox@gmail.com>
2024-08-07 13:36:47 +0330

    day33

--- day26-list-comprehension/alphabet_project/main.py
+++ day26-list-comprehension/alphabet_project/main.py
@@ -1,6 +1,16 @@
 import pandas
 data=pandas.read_csv("nato_phonetic_alphabet.csv")
 alphabet_dict={row.letter:row.code for (index,row) in data.iterrows()}
-word=input("Enter a word: ").upper()
-output_list={letter:alphabet_dict[letter] for letter in word}
-print(output_list)
+
+
+
+def generate_phonetic():
+    word=input("Enter a word: ").upper()
+    try:
+        output_list = {letter: alphabet_dict[letter] for letter in word}
+    except KeyError:
+        print("Sorry you can just enter letters")
+        generate_phonetic()
+    else:
+        print(output_list)
+generate_phonetic()
