170fc2fd6c9d965048f59a900e2b4a34369b7243
thesheox <thesheox@gmail.com>
2024-08-08 14:38:03 +0330

    day37

--- day33-api/api_main.py
+++ day33-api/api_main.py
@@ -1,4 +1,6 @@
 import requests
+
+
 response=requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
 data=response.json()
--- day34-quiz-app/quiz_app.py
+++ day34-quiz-app/quiz_app.py
@@ -1,3 +1,78 @@
+
+import requests
+parameters={
+    "amount":10,
+    "type":"boolean",
+    "category":18
+}
+response=requests.get(url="https://opentdb.com/api.php",params=parameters)
+response.raise_for_status()
+question_data =response.json()["results"]
+
+
+
+
+
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+from ui import QuizIntreface
+
+question_bank = []
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+
+quiz = QuizBrain(question_bank)
+quiz_ui=QuizIntreface(quiz)
+
+# while quiz.still_has_questions():
+#     quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+
+
+class Question:
+
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
+
+
+import html
+class QuizBrain:
+
+    def __init__(self, q_list):
+        self.question_number = 0
+        self.score = 0
+        self.question_list = q_list
+        self.current_question = None
+
+    def still_has_questions(self):
+        return self.question_number < len(self.question_list)
+
+    def next_question(self):
+        self.current_question = self.question_list[self.question_number]
+        self.question_number += 1
+
+        q_text=html.unescape(self.current_question.text)
+
+        return f"Q.{self.question_number}: {q_text}"
+        self.check_answer(user_answer)
+
+    def check_answer(self, user_answer):
+        correct_answer = self.current_question.answer
+        if user_answer.lower() == correct_answer.lower():
+            self.score += 1
+            return True
+        else:
+            return False
+
+
 THEME_COLOR = "#375362"
 from tkinter import*
 from quiz_brain import QuizBrain
